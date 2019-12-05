a8d4462d1f71c5d7ba0a9a9578670cbcfd4641e8
SomeMushroom <somemushroom@protonmail.com>
2019-12-05 16:05:26 -0500

    Possible implementation of StatefulIcon - needs testing

---
+++ IO/Components/StatefulIcon.h
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////////////
+//	This file is part of the continued Journey MMORPG client					//
+//	Copyright (C) 2015-2019  Daniel Allendorf, Ryan Payton						//
+//																				//
+//	This program is free software: you can redistribute it and/or modify		//
+//	it under the terms of the GNU Affero General Public License as published by	//
+//	the Free Software Foundation, either version 3 of the License, or			//
+//	(at your option) any later version.											//
+//																				//
+//	This program is distributed in the hope that it will be useful,				//
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of				//
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the				//
+//	GNU Affero General Public License for more details.							//
+//																				//
+//	You should have received a copy of the GNU Affero General Public License	//
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.		//
+//////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "Icon.h"
+
+namespace ms
+{
+	class StatefulIcon : public Icon
+	{
+	public:
+		StatefulIcon(std::unique_ptr<Icon::Type> type, Texture normal_tx, Texture disabled_tx, Texture mouseover_tx);
+
+		Texture get_texture() const override;
+
+		void set_state(Icon::State state);
+	private:
+		Icon::State state;
+	};
+}
